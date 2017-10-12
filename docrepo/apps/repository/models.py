cc99df0a8f845571ecb33d98bec92c9713207e9d
Harlin Seritt <harlin.seritt@gmail.com>
2017-10-11 20:15:34 -0600

    Fixed Issue #2 - Create folder model in repository app

--- docrepo/apps/repository/models.py
+++ docrepo/apps/repository/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Folder(models.Model):
+    """Folder model - contains documents and other folders."""
+
+    name = models.CharField('Name', max_length=255)
+    title = models.CharField('Title', max_length=255, null=True, blank=True)
+    description = models.TextField('Description', null=True, blank=True)
+    created = models.DateTimeField('Created', auto_now_add=True)
+    modified = models.DateTimeField('Modified', auto_now=True)
+
+    def __str__(self):
+        return self.name
--- docrepo/apps/repository/tests.py
+++ docrepo/apps/repository/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+"""Tests module for the repository app."""
 
-# Create your tests here.
+from django.test import TestCase
+from .models import Folder
+
+
+class RepositoryModelsTestCase(TestCase):
+    """TestCase class for Repository app models."""
+
+    def setUp(self):
+        pass
+
+    def test_add_folder_model(self):
+        """Test for adding Folder model."""
+        self.assertTrue(
+            Folder.objects.create(
+                name='Folder1', title='Folder #1', description='This is folder #1'
+            )
+        )
+
+        folder = Folder.objects.get(name='Folder1')
+        self.assertEqual(folder.name, 'Folder1')
+        self.assertEqual(folder.title, 'Folder #1')
+        self.assertEqual(folder.description, 'This is folder #1')
