1b57206e76790bfadb2052e8998f8100df0fef04
Harlin Seritt <harlin.seritt@gmail.com>
2017-10-20 15:00:34 -0600

    Redo models - Change model scheme for docrepo

--- docrepo/apps/repository/admin.py
+++ docrepo/apps/repository/admin.py
@@ -1,8 +1,12 @@
 """Admin module configs and registrations app models."""
 
 from django.contrib import admin
-from .models import Document, Folder
+from .models import Tenant, Project, Category, Document, Facet, Property
 
 
-admin.site.register(Folder)
+admin.site.register(Tenant)
+admin.site.register(Project)
+admin.site.register(Category)
 admin.site.register(Document)
+admin.site.register(Facet)
+admin.site.register(Property)
--- docrepo/apps/repository/models.py
+++ docrepo/apps/repository/models.py
@@ -2,37 +2,93 @@
 from django.db import models
 
 
-class ContentNode(models.Model):
-    """Abstract model"""
-    name = models.CharField('Name', max_length=255)
-    title = models.CharField('Title', max_length=255, null=True, blank=True)
+class NamedModel(models.Model):
+    name = models.CharField('Name', max_length=50, unique=True)
     description = models.TextField('Description', null=True, blank=True)
-    created = models.DateTimeField('Created', auto_now_add=True)
-    modified = models.DateTimeField('Modified', auto_now=True)
 
     class Meta:
         abstract = True
 
 
-class Folder(ContentNode):
-    """Folder model - contains documents and other folders."""
-    parent = models.ForeignKey('self', null=True, blank=True)
+class TimestampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = 'Folder'
-        verbose_name_plural = 'Folders'
+        abstract = True
+
+
+class Tenant(NamedModel, TimestampedModel):
+    domain = models.CharField('Domain Name', max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = 'Tenant'
+        verbose_name_plural = 'Tenants'
 
     def __str__(self):
         return self.name
 
 
-class Document(ContentNode):
-    """Document model - represents a document and the versions associated with it."""
-    parent = models.ForeignKey('Folder')
+class Project(NamedModel, TimestampedModel):
+    parent_tenant = models.ForeignKey('Tenant')
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(NamedModel, TimestampedModel):
+    parent_project = models.ForeignKey('Project')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Document(NamedModel, TimestampedModel):
+    parent_categories = models.ManyToManyField('Category')
+    title = models.CharField('Title', max_length=255, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Document'
         verbose_name_plural = 'Documents'
+
+    def __str__(self):
+        return self.name
+
+
+class Facet(NamedModel):
+    document = models.ForeignKey('Document')
+
+    class Meta:
+        verbose_name = 'Facet'
+        verbose_name_plural = 'Facets'
+
+    def __str__(self):
+        return self.name
+
+
+class Property(models.Model):
+    facet = models.ForeignKey('Facet')
+    name = models.CharField('Name', max_length=80)
+    value = models.TextField('Value')
+    data_type = models.CharField('Data Type', max_length=30, choices=(
+            ('int', 'int'),
+            ('float', 'float'),
+            ('string', 'string'),
+            ('datetime', 'datetime'),
+        )
+    )
+
+    class Meta:
+        verbose_name = 'Property'
+        verbose_name_plural = 'Properties'
 
     def __str__(self):
         return self.name
